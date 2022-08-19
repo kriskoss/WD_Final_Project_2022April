3c6a2dd6ede97f6d8f2818b4846243c4706aff04
kriskoss <64606271+kriskoss@users.noreply.github.com>
2022-08-19 17:24:29 +0200

    cafes added, short list modified

--- scripts/data__cafes.js
+++ scripts/data__cafes.js
@@ -1,12 +1,13 @@
 export const path__nero="Resources/Images/common/Cafe__logo/nero__logo.png"
 export const path__costa="Resources/Images/common/Cafe__logo/costa__logo.png"
 export const path__starbucks="Resources/Images/common/Cafe__logo/starbucks__logo.png"
+export const path__etno="Resources/Images/common/Cafe__logo/etno__logo.jpg"
 
 
 export const data = {
   title: "Best cafes",
   cafes: [
-    {name:"Nero Cafe - Nowy Świat",
+    {name:"Nero - Nowy Świat",
      img_path:"Resources/Images/cafes/alley.jpg",
      logo_path: path__nero,
      description:"Always busy cafe in the main turistic street of Warsaw, the Nowy Świat Street",
@@ -22,10 +23,57 @@ export const data = {
      logo_path: path__costa,
      description:"Cafe next to the Pola Mokotowskie park and also Szkoła Główna Handlowa - one of the top economic universities in Poland",},
 
-    {name:"Nero Cafe - pl.Trzech Krzyży",
+    {name:"Nero - pl.Trzech Krzyży",
      img_path:"Resources/Images/cafes/nero_unii.jpg",
      logo_path: path__nero,
      description:"Always busy cafe close to the city center area. Many restaurants in the vicinity",},
+    
+    {name:"Etno - Marszalkowska",
+     img_path:"Resources/Images/cafes/etno__marszalkowska.jpg",
+     logo_path: path__etno,
+     description:"Spacious and bright interior. Plenty tables to seat.",},
+    
+    {name:"Nero - Astoria",
+     img_path:"Resources/Images/cafes/nero__astoria.jpg",
+     logo_path: path__nero,
+     description:"2-floors cafe. Nice view to the Palac Kultury i Nauki.",},
+    
+    {name:"Nero - bulwary",
+     img_path:"Resources/Images/cafes/nero__bulwary.jpg",
+     logo_path: path__nero,
+     description:"Just next to the Vistula river. You can see the water almost next to the windows",},
+    
+    {name:"Nero - Piekna and Krucza corner",
+     img_path:"Resources/Images/cafes/nero__piekna_krucza.jpg",
+     logo_path: path__nero,
+     description:"Not too large, but in the more quiet part of the city",},
+    
+    {name:"Nero - Plac Konstyucji",
+     img_path:"Resources/Images/cafes/nero__pl_konstytucji.jpg",
+     logo_path: path__nero,
+     description:"Placed in one of the major areas of the city, near Plac Kostytucji. You can see large monuments outside. Also, it is short walk from lively Plac Zbawiciela.",},
+    
+    {name:"Nero - Rondo ONZ",
+     img_path:"Resources/Images/cafes/nero__rondo_onz.jpg",
+     logo_path: path__nero,
+     description:"Large, two-floor cafe surrounded by skyscreapers. Always busy.",},
+    
+    {name:"Nero - Swietokrzyska and Nowy Swiat corner",
+     img_path:"Resources/Images/cafes/nero__swietokrzyska.jpg",
+     logo_path: path__nero,
+     description:"Cosy cafe next to the lively Nowy Świat street.",},
+    
+    {name:"Nero - Telimena",
+     img_path:"Resources/Images/cafes/nero__telimena.jpg",
+     logo_path: path__nero,
+     description:"Oldest cafe location in Warsaw. Fameous Polish writer used to spend time there at the begining of XVII century.",},
+   
+    
+//    {name:"",
+//     img_path:"Resources/Images/cafes/.jpg",
+//     logo_path: path__nero,
+//     description:"",},
+//    
   ]
 };
 
